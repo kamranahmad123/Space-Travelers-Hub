600249098154b377b4046cf5ef0db42de59b767e
kamranahmad123 <kamrankhan.ahmad2@yahoo.com>
2023-05-05 20:55:37 +0430

    make changes

--- src/components/features/_tests_/rocket.test.js
+++ src/components/features/_tests_/rocket.test.js
@@ -3,6 +3,7 @@ import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import store from '../../../redux/store';
 import Rockets from '../../rockets/rockets';
+import '@testing-library/jest-dom';
 
 describe('performing test for rockets', () => {
   it('testing if rockets page really matches', () => {

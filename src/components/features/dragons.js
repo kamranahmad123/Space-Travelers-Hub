0058e221f615b4c8289da8c90f0b5769be9501e2
FrankMugagga <frank.mugagga@gmail.com>
2023-05-05 13:32:13 +0300

    Work on tests

--- src/components/features/dragons.js
+++ src/components/features/dragons.js
@@ -1,40 +1,67 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getDragonData } from '../../redux/dragons/dragonsSlice';
+import { getDragonData, reserveD, cancelD } from '../../redux/dragons/dragonsSlice';
 import '../styles/dragonStyle.css';
 
 function Dragons() {
-  const { dragons, loading } = useSelector((state) => state.dragons);
-  const dispatch = useDispatch();
+const dragons = useSelector((state) => state.dragons);
+const loading = useSelector((state) => state.dragons.loading);
 
-  useEffect(() => {
-    dispatch(getDragonData());
-  }, [dispatch]);
+const dispatch = useDispatch();
 
-  if (loading) {
-    return (
-      <div>Loading</div>
-    );
-  }
-  return (
-    <ul>
-      {
-            dragons.map((dragon) => (
-              <li className="dragonCont" key={dragon.id}>
-                <div>
-                  <img className="dragonimg" alt="dragons" src={dragon.flickr_images} />
-                </div>
-                <div className="dragonDetails">
-                  <h3>{dragon.name}</h3>
-                  <h4>{dragon.type}</h4>
-                  <div>{dragon.description}</div>
-                  <button className="dragonBtn" type="submit">Reserve Dragon</button>
-                </div>
-              </li>
-            ))
-        }
-    </ul>
-  );
+useEffect(() => {
+dispatch(getDragonData());
+}, [dispatch]);
+
+const handleReserveBtn = (id) => {
+dispatch(reserveD(id));
+};
+const handleCancelBtn = (id) => {
+dispatch(cancelD(id));
+};
+
+if (loading) {
+return <div>loading....</div>;
+}
+
+return (
+<ul>
+{
+dragons.map((dragon) => (
+<li className="dragonCont" key={dragon.id}>
+<div>
+<img className="dragonimg" alt="dragons" src={dragon.flickr_images} />
+</div>
+<div className="dragonDetails">
+<h3>
+{dragon.name}
+{dragon.reserved}
+</h3>
+<h4>{dragon.type}</h4>
+
+<div className="container_dragons">
+{dragon.reserved && <p className="reserved">Reserved</p>}
+<p>{dragon.description}</p>
+</div>
+{/* <span>
+<p className="reserved">{dragon.reserved && <p>'Reserved'</p>}</p>
+{' '}
+{dragon.description}
+</span> */}
+
+{!dragon.reserved && <button onClick={() => handleReserveBtn(dragon.id)} className="dragonBtn" type="submit">Reserve Dragon</button> }
+{dragon.reserved && <button onClick={() => handleCancelBtn(dragon.id)} className="cancelBtn" type="submit">Cancel Reservation</button> }
+</div>
+</li>
+))
+}
+</ul>
+);
 }
 
 export default Dragons;
+
+
+
+
+
